f621ae62942feade02bd3ffde97e0bb42585d66d
charasullia <charanrajr713@gmail.com>
2021-10-23 10:40:30 +0530

    [charansullia]added:option

--- TikTokToe/TikTokToe/Game.cs
+++ TikTokToe/TikTokToe/Game.cs
@@ -6,18 +6,39 @@ namespace TikTokToe
 {
    public class Game
     {
-        public char[,] gameboard = new char[10, 10];
-        public void tik()
+        public char player;
+        public char computer;
+        //public char[] gameboard = new char[10,10];
+        //public void tik()
+        //{
+        //    Console.WriteLine("A B C\n");
+        //    for(int i=0;i<10;i++)
+        //    {
+        //        for (int j = 0; j < 10; j++)
+        //        {
+        //            Console.Write("|" + gameboard[i, j]);
+        //        }
+        //        Console.Write("\n\n");
+        //    }
+        //}
+        public void chooseoption()
         {
-            Console.WriteLine("A B C\n");
-            for(int i=0;i<10;i++)
+            Console.WriteLine("enter option X or Y");
+            char option = Convert.ToChar(Console.ReadLine());
+            switch(option)
             {
-                for(int j=0;j<10;j++)
-                {
-                    Console.Write("|"+gameboard[i,j]);
-                }
-                Console.Write("\n\n");
+                case 'X':
+                    Console.WriteLine("player");
+                    break;
+                case 'O':
+                    Console.WriteLine("computer");
+                    break;
+                default:
+                    Console.WriteLine("invalid option");
+                    break;
             }
         }
+
+
     }
 }
--- TikTokToe/TikTokToe/Program.cs
+++ TikTokToe/TikTokToe/Program.cs
@@ -8,7 +8,7 @@ namespace TikTokToe
         {
             Console.WriteLine("Hello World!");
             Game obj = new Game();
-            obj.tik();
+            obj.chooseoption();
         }
     }
 }
