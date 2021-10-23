b4adc348ffd0ddc84831d370b24f9bf9e6550585
charasullia <charanrajr713@gmail.com>
2021-10-23 09:27:14 +0530

    [charansullia]added: initialize

---
+++ TikTokToe/TikTokToe/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TikTokToe
+{
+   public class Game
+    {
+        public char[,] gameboard = new char[10, 10];
+        public void tik()
+        {
+            Console.WriteLine("A B C\n");
+            for(int i=0;i<10;i++)
+            {
+                for(int j=0;j<10;j++)
+                {
+                    Console.Write("|"+gameboard[i,j]);
+                }
+                Console.Write("\n\n");
+            }
+        }
+    }
+}
---
+++ TikTokToe/TikTokToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TikTokToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Game obj = new Game();
+            obj.tik();
+        }
+    }
+}
